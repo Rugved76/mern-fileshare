ccab3d0cd8bb47703cae640fdeb55e3f8879eb83
Rugved Wagh <89897353+Rugved76@users.noreply.github.com>
2023-06-14 12:39:41 +0530

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 // const API_URI = 'http://localhost:8000';
-const API_URI = https://fileshr.onrender.com
+// const API_URI = https://fileshr.onrender.com
 
 export const uploadFile = async (data) => {
     try {
-        const response = await axios.post(`${API_URI}/upload`, data);
+        const response = await axios.post(`https://fileshr.onrender.com/upload`, data);
         return response.data;
     } catch (error) {
         console.log('Error while calling the API ', error.message);
